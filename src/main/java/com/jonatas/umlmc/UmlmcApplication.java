50c73cef44304f4fbeffaff036e55b8202cc96e8
Jonatas Mielnik <jonatasmielnik@gmail.com>
2018-09-17 16:23:01 -0300

    Projeto criado

---
+++ src/main/java/com/jonatas/umlmc/UmlmcApplication.java
@@ -0,0 +1,12 @@
+package com.jonatas.umlmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmlmcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jonatas/umlmc/UmlmcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jonatas.umlmc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UmlmcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
